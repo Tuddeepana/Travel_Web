2ba0095ee961c619f97ef6512d4ef433c7316f98
DhananjayaDilz <dhananjaya.20221596@iit.ac.lk>
2024-12-18 01:06:03 +0530

    update contact form section

--- src/components/ContactFormSection.jsx
+++ src/components/ContactFormSection.jsx
@@ -1,51 +1,79 @@
+import { IoCall } from "react-icons/io5";
+import { IoIosMail } from "react-icons/io";
+import { FaTwitter } from "react-icons/fa";
+import { AiFillLinkedin } from "react-icons/ai";
+import { ImFacebook2 } from "react-icons/im";
+import { FaInstagramSquare } from "react-icons/fa";
 
 import "./ContactFormSection.css";
 
 const ContactFormSection = () => {
     return (
       <div className="contact-form-section">
-        <h1>Let’s Plan your Ideal Vacation</h1>
-        <p>Fill up the form below and have our agent help you with your ideal travel in Sri Lanka.</p>
-  
-        <form className="form">
-          <div className="form-row">
-            <input type="text" placeholder="Name" className="form-input" />
-            <input type="email" placeholder="Email" className="form-input" />
-          </div>
-          <div className="form-row">
-            <input type="text" placeholder="Phone" className="form-input" />
-            <select className="form-input">
-              <option value="">Select Service</option>
-              <option value="tour">Tour</option>
-              <option value="accommodation">Accommodation</option>
-              <option value="transport">Transport</option>
-            </select>
-          </div>
-          <textarea
-            placeholder="Travel Brief..."
-            className="form-textarea"
-          ></textarea>
-          <button type="submit" className="form-button">Send Message</button>
-        </form>
+        <div className="left-sub-container">
+          <h1>Let’s Plan your Ideal Vacation</h1>
+          <p>Fill up the form below and have our agent help you with your ideal travel in Sri Lanka.</p>
+    
+          <form className="form">
+            <div className="form-row">
+              <input type="text" placeholder="Name" className="form-input" />
+              <input type="email" placeholder="Email" className="form-input" />
+            </div>
+            <div className="form-row">
+              <input type="text" placeholder="Phone" className="form-input" />
+              <select className="form-input">
+                <option value="">Select Service</option>
+                <option value="tour">Tour</option>
+                <option value="accommodation">Accommodation</option>
+                <option value="transport">Transport</option>
+              </select>
+            </div>
+            <textarea
+              placeholder="Travel Brief..."
+              className="form-textarea"
+            ></textarea>
+            <button type="submit" className="form-button">Send Message</button>
+          </form>
+        </div>
   
         <div className="contact-details">
-          <div className="contact-item">
-            <i className="icon-phone"></i>
+          <div className="contact-item ">
+            <div className="  border rounded-md border-teal-700  ">
+            <IoCall size={35} className="items-center m-2  "/>
+            </div>
+            <div>
             <p>Give Us a Call</p>
             <p>+91 098 098 8333</p>
+            </div>
           </div>
-          <div className="contact-item">
-            <i className="icon-email"></i>
+
+          <div className="contact-item ">
+            <div className="  border rounded-md border-teal-700  ">
+            <IoIosMail  size={35} className="items-center m-2  "/>
+            </div>
+            <div>
             <p>Send Us an Email</p>
             <p>ThisaTravel@Gmail.com</p>
+            </div>
           </div>
+
+         
           <div className="social-icons">
-            <i className="icon-twitter"></i>
-            <i className="icon-linkedin"></i>
-            <i className="icon-instagram"></i>
-            <i className="icon-facebook"></i>
+            <div className="  border rounded-md border-teal-700  ">
+              <FaTwitter  size={15} className="items-center m-2  "/>
+            </div>
+            <div className="  border rounded-md border-teal-700  ">
+            <AiFillLinkedin  size={15} className="items-center m-2  "/>
+          </div>
+          <div className="  border rounded-md border-teal-700  ">
+            <ImFacebook2  size={15} className="items-center m-2  "/>
+          </div>
+          <div className="  border rounded-md border-teal-700  ">
+            <FaInstagramSquare  size={15} className="items-center m-2  "/>
+          </div>
           </div>
         </div>
+
       </div>
     );
   };
