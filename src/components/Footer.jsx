eb0961f661c0882c4fbcfb57c1fe746ddee6f82d
Tuddeepana <tauddeepana@gmail.com>
2024-12-18 14:33:58 +0530

    footer responsive fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,47 +4,46 @@ import { ImFacebook2 } from "react-icons/im";
 import { FaInstagramSquare } from "react-icons/fa";
 
 import './Footer.css';
+
 const Footer = () => {
     return (
-      <footer className="footer">
-        <div className="footer-logo-container">
-          <h2>LOGO</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
-          <div className="social-icons">
-            
-            <FaTwitter  size={20} className="items-center m-2  "/>
-            <AiFillLinkedin  size={20} className="items-center m-2  "/>
-            <ImFacebook2  size={20} className="items-center m-2  "/>
-            <FaInstagramSquare  size={20} className="items-center m-2  "/>
-            
-          </div>
-        </div>
-  
-        <div className="footer-links">
-          <h4>Quick Links</h4>
-          <ul>
-            <li>Top Destinations</li>
-            <li>Our Packages</li>
-            <li>Top Activities</li>
-          </ul>
-        </div>
-  
-        <div className="footer-links">
-          <h4>Company</h4>
-          <ul>
-            <li>How are we</li>
-            <li>Terms of service</li>
-          </ul>
-        </div>
-  
-        <div className="footer-contact">
-          <h4>Contact Us</h4>
-          <p>Location Location Location...</p>
-          <p>+94 123 456 7890</p>
-          <p>thisatravel@mail.com</p>
-        </div>
-      </footer>
-    );
-  };
+        <footer className="footer">
+            <div className="footer-logo-container">
+                <h2>LOGO</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+                <div className="social-icons">
+                    <FaTwitter size={20} className="m-2" />
+                    <AiFillLinkedin size={20} className="m-2" />
+                    <ImFacebook2 size={20} className="m-2" />
+                    <FaInstagramSquare size={20} className="m-2" />
+                </div>
+            </div>
 
-  export default Footer;
+            <div className="footer-links">
+                <h4>Quick Links</h4>
+                <ul>
+                    <li>Top Destinations</li>
+                    <li>Our Packages</li>
+                    <li>Top Activities</li>
+                </ul>
+            </div>
+
+            <div className="footer-links">
+                <h4>Company</h4>
+                <ul>
+                    <li>Who are we</li>
+                    <li>Terms of service</li>
+                </ul>
+            </div>
+
+            <div className="footer-contact">
+                <h4>Contact Us</h4>
+                <p>Location Location Location...</p>
+                <p>+94 123 456 7890</p>
+                <p>thisatravel@mail.com</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
